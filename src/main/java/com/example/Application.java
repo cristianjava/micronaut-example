37a05186e62654ec8832fb7a0be4b01db6935154
CEIBA\cristian.castano <cristian.castano@ceiba.com.co>
2021-11-20 21:37:06 -0500

    :sparkles:Commit configutarion swagger

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,9 +3,20 @@ package com.example;
 import com.example.services.HelloWorldService;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "micronaut application",
+                version = "0.1",
+                description = "integration with AWS",
+                license = @License(name = "MIT")
+        )
+)
 public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
